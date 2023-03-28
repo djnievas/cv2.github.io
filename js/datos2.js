b653d0ebc37d25baa9702e00b9beee224fd12cd7
djnievas <126597782+djnievas@users.noreply.github.com>
2023-03-27 22:07:18 -0300

    upload index.html instead

---
+++ js/datos2.js
@@ -0,0 +1,4 @@
+document.getElementById('botontel').addEventListener('click', function () {
+    console.log("Tel: 1137764586");
+    document.getElementById("tel").innerHTML = "Celular: 1135668217";
+});
---
+++ js/datosopcionales.js
@@ -0,0 +1,9 @@
+
+document.getElementById('boton-color').addEventListener('click', function () {
+    document.body.style.backgroundColor ='#F6FF33';
+});
+
+document.getElementById('boton-default').addEventListener('click', function () {
+    document.body.style.backgroundColor ='aqua';
+});
+
